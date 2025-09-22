afaef640f25d7b487100ec8f6bf86f1b4f3eca00
genario100 <76125969+genario100@users.noreply.github.com>
2025-09-22 13:50:32 -0300

    Adicionar arquivos de projeto.

---
+++ App.xaml.cs
@@ -0,0 +1,22 @@
+﻿namespace MauiAppHotel
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+
+        protected override Window CreateWindow(IActivationState activationState)
+        {
+            var window = base.CreateWindow(activationState);
+            window.Title = "Hotel Management App";
+            window.Width = 400;
+            window.Height = 700;
+
+            return window;
+        }
+    }
+}
