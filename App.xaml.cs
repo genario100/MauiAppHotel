1442b21b8b0f266b5b8e3902d583ae46af9984a9
genario100 <76125969+genario100@users.noreply.github.com>
2025-09-25 17:22:30 -0300

    atualizacaofinal

--- App.xaml.cs
+++ App.xaml.cs
@@ -6,15 +6,15 @@
         {
             InitializeComponent();
 
-            MainPage = new AppShell();
+            MainPage = new NavigationPage( new Views.ContratacaoHospedagem());
         }
 
         protected override Window CreateWindow(IActivationState activationState)
         {
             var window = base.CreateWindow(activationState);
             window.Title = "Hotel Management App";
-            window.Width = 400;
-            window.Height = 700;
+            window.Width = 450;
+            window.Height = 900;
 
             return window;
         }
